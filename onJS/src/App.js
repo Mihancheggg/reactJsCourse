e13e94ec3e883b00923d51c32a3ef1dd655a84d1
Mihancheggg <schutow.mihail@yandex.ru>
2022-05-25 10:33:56 +0300

    doubled

---
+++ onJS/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header.js';
+import Technologies from './Technologies.js';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Technologies />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ onJS/src/Technologies.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Technologies = () => {
+    return(
+        <div className="App">
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JS</li>
+          <li>React</li>
+        </ul>
+      </div>
+  
+    )
+}
+
+export default Technologies;
